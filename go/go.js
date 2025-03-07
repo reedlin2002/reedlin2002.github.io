669030eda6454a34a2afb7ca94401d2d5192c615
Lin <91250398+reedlin2002@users.noreply.github.com>
2025-03-07 21:10:52 +0800

    Update go.js

--- go/go.js
+++ go/go.js
@@ -1,44 +1,36 @@
-let bg; // 用來存背景圖片
-
-function preload() {
-    bg = loadImage('background.jpg'); // 載入背景圖
-}
-
 function setup() {
     initializeFields();
     createCanvas(500, 500);
 }
 
+// 棋子的座標 陣列
 var bx;
+
+// 棋子的座標 陣列
 var by;
+
+// 棋子的數字,一開始0個
 var N;
 
 function draw() {
-    background(bg); // 設定背景圖
-    drawBoard();
-    drawPieces();
-    drawHoverPiece();
-}
-
-function drawBoard() {
-    stroke(0); // 設定線條顏色
-    for (let y = 50; y < 500; y += 50) {
+    background(color(0xFF, 0xEF, 0xD5));
+    for (var y = 50; y < 500; y += 50) {
         line(0, y, 500, y);
     }
-    for (let x = 50; x < 500; x += 50) {
+    for (var x = 50; x < 500; x += 50) {
         line(x, 0, x, 500);
     }
-}
-
-function drawPieces() {
-    for (let i = 0; i < N; i++) {
-        fill(i % 2 === 0 ? 0 : 255); // 黑白交替
+    for (var i = 0; i < N; i++) {
+        if (i % 2 == 0)
+            fill(0);
+        else
+            fill(255);
         ellipse(bx[i], by[i], 40, 40);
     }
-}
-
-function drawHoverPiece() {
-    fill(N % 2 === 0 ? 0 : 255); // 當前滑鼠棋子顏色
+    if (N % 2 == 0)
+        fill(0);
+    else
+        fill(255);
     ellipse(mouseX, mouseY, 40, 40);
 }
 
